a6564f87b9565a5b813e70f8d29fcee4dc40b589
Elizabeth Beadle (s5109333) <s5109333@mac38571.mac.bournemouth.ac.uk>
2019-04-02 12:25:46 +0100

    Initial Commit

---
+++ Group B- Group App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Group B- Group App
+//
+//  Created by Elizabeth Beadle (s5109333) on 02/04/2019.
+//  Copyright © 2019 Elizabeth Beadle (s5109333). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
