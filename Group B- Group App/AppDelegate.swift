c1457c6c1c835ce4f9c7f3a3408504791f0035c9
Elizabeth Beadle (s5109333) <s5109333@mac38572.mac.bournemouth.ac.uk>
2019-04-30 12:57:33 +0100

    update

--- Group B- Group App/AppDelegate.swift
+++ Group B- Group App/AppDelegate.swift
@@ -1,10 +1,4 @@
-//
-//  AppDelegate.swift
-//  Group B- Group App
-//
-//  Created by Elizabeth Beadle (s5109333) on 02/04/2019.
-//  Copyright © 2019 Elizabeth Beadle (s5109333). All rights reserved.
-//
+
 
 import UIKit
 
